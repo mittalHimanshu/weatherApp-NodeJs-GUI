8b4e4440c27e667e920e196c4cd55fd95d80caa6
Himanshu Mittal <mittalhimanshu121@gmail.com>
2018-12-22 19:07:52 +0530

    deploying

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ var router = require('./router');
 http.createServer((request, response) => {
     router.home(request, response);
     router.user(request, response);
-}).listen(process.env.PORT || 3000, '127.0.0.1');
+}).listen(process.env.PORT || 3000);
 console.log('server running');
